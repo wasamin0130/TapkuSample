692824d8af3c16b8642e6c3deacbec7ca94bef95
wasamin0130 <wasamin0130@gmail.com>
2012-09-30 01:07:59 +0900

    Initial commit

--- TapkuSample/TapkuSampleViewController.m
+++ TapkuSample/TapkuSampleViewController.m
@@ -7,23 +7,46 @@
 //
 
 #import "TapkuSampleViewController.h"
+#import <TapkuLibrary/TKCalendarMonthView.h>
 
-@interface TapkuSampleViewController ()
+@interface TapkuSampleViewController () <TKCalendarMonthViewDelegate>
+
+@property (nonatomic, strong) TKCalendarMonthView *calendarView;
+@property (nonatomic, strong) NSDate *dateSelected;
 
 @end
 
 @implementation TapkuSampleViewController
 
+@synthesize calendarView = _calendarView;
+@synthesize dateSelected = _dateSelected;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // カレンダービューを追加
+    self.calendarView = [[TKCalendarMonthView alloc] init];
+    self.calendarView.delegate = self;
+    [self.view addSubview:self.calendarView];
+    [self.calendarView reload];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc
+{
+    self.calendarView = nil;
+}
+
+-(void)calendarMontView:(TKCalendarMonthView *)monthView didSelectDate:(NSDate *)date
+{
+    self.dateSelected = date;	
 }
 
 @end
